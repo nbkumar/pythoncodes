13efd0576a03a0ff2af3453024a127458e0cef5b
Naveen Kumar BT <btnaveenkumar@gmail.com>
2018-07-23 20:16:17 +0530

    Update python_io.py

--- python_io.py
+++ python_io.py
@@ -1,3 +1,6 @@
-print("statement)
+print("statement")
 print(a)
 #print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+print('The value of x is {} and y is {}'.format(x,y))
+print('Hello {name}, {greeting}'.format(greeting = 'Goodmorning', name = 'John'))
+num = input('Enter a number: ')
